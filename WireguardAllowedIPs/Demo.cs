31900de2af11caeef4bdaf788ebac5acb207594c
fabyr <fabyr@proton.me>
2023-09-27 14:37:16 +0200

    Add Demo.cs (Used in README)

---
+++ WireguardAllowedIPs/Demo.cs
@@ -0,0 +1,26 @@
+// README demo
+using WireguardAllowedIPs.Core;
+
+namespace WireguardAllowedIPs;
+
+internal static class Demo
+{
+    public static void Run()
+    {
+        string[] allowedIPs = new[] {
+            "0.0.0.0/0",
+            "::/0"
+        };
+        string[] disallowedIPs = new[] {
+            "10.0.0.0/8",
+            "192.168.0.0/16",
+            "172.16.0.0/12",
+            "72.8.99.100/32"
+        };
+
+        // The IP-Types have implementations for ToString
+        IPNetwork[] result = Calculator.CalculateAllowedIPs(allowedIPs, disallowedIPs);
+
+        Console.WriteLine($"AllowedIPs = {string.Join<IPNetwork>(",", result)}");
+    }
+}
