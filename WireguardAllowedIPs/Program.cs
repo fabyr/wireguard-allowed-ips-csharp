183b6460a0e69514862f0f6ffda776ed19d01990
fabyr <fabyr@proton.me>
2023-09-27 11:36:43 +0200

    Add CLI

--- WireguardAllowedIPs/Program.cs
+++ WireguardAllowedIPs/Program.cs
@@ -1,4 +1,5 @@
-﻿using WireguardAllowedIPs.Core;
+﻿using System.Net;
+using WireguardAllowedIPs.Core;
 
 namespace WireguardAllowedIPs;
 
@@ -6,11 +7,108 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string[] allowed = new[] { "0.0.0.0/0", "::0/0" };
-        string[] disallowed = new[] { "10.1.0.0/16", "127.0.0.0/8", "169.254.0.0/16", "172.16.0.0/12", "192.168.0.0/16", "146.70.116.98/32" };
+        static void PrintHelp()
+        {
+            Console.WriteLine("Usage: wireguard-allowed-ips [-h] [-a \"all\"|allowed-ranges] [-d disallowed-ranges]");
+            Console.WriteLine();
+            Console.WriteLine("Calculate the allowed ranges from an underlying allowed range");
+            Console.WriteLine("and disallowed ranges within that range.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine(" -h, --help\t\tDisplays this help.");
+            Console.WriteLine();
+            Console.WriteLine(" -a, --allowed\t\tSet the allowed ip ranges in CIDR notation, separated by commas.");
+            Console.WriteLine("\t\t\tDefault Value if omitted: All IPs are allowed (both IPv4 and IPv6).");
+            Console.WriteLine();
+            Console.WriteLine(" -d, --disallowed\tSet the disallowed ip ranges in CIDR notation, separated by commas.");
+        }
 
-        IPNetwork[] calculated = Calculator.CalculateAllowedIPs(allowed, disallowed);
+        if(args.Length == 0)
+        {
+            PrintHelp();
+            return;
+        }
 
-        Console.WriteLine($"AllowedIPs = {string.Join<IPNetwork>(",", calculated)}");
+        string[] allowedIps = new string[] { };
+        string[] disallowedIps = new string[] { };
+
+        for(int i = 0; i < args.Length; i++)
+        {
+            string arg = args[i];
+            if(arg.StartsWith("-"))
+            {
+                string[] innerArgList;
+                if(arg.StartsWith("--"))
+                    innerArgList = new string[1] { arg[2..] };
+                else
+                    // a single dash corresponds to single character arguments afterwards
+                    innerArgList = arg[1..].Select(x => x.ToString()).ToArray();
+                
+                foreach(string innerArg in innerArgList)
+                {
+                    switch(innerArg)
+                    {
+                        case "help" or "h":
+                            PrintHelp();
+                            return;
+                        case "allowed" or "a":
+                            if(i == args.Length - 1)
+                            {
+                                Console.WriteLine($"Missing allowed IPs argument! (after '{innerArg}')");
+                                return;
+                            }
+                            allowedIps = args[++i].Split(',');
+                            break;
+                        case "disallowed" or "d":
+                            if(i == args.Length - 1)
+                            {
+                                Console.WriteLine($"Missing disallowed IPs argument! (after '{innerArg}')");
+                                return;
+                            }
+                            disallowedIps = args[++i].Split(',');
+                            break;
+                        default:
+                            Console.WriteLine($"Unknown argument '{innerArg}'. Use --help for help.");
+                            return;
+                    }
+                }
+            }
+        }
+        
+        if(allowedIps.Length == 0)
+            allowedIps = new[] { "all" }; // Default is allow all
+
+        if(allowedIps.Length == 1 && allowedIps[0] == "all")
+            allowedIps = new[] { "0.0.0.0/0", "::/0" };
+        
+        IPNetwork[] allowed = new IPNetwork[allowedIps.Length];
+        IPNetwork[] disallowed = new IPNetwork[disallowedIps.Length];
+
+        for(int i = 0; i < allowed.Length; i++)
+        {
+            try
+            {
+                allowed[i] = IPNetwork.Parse(allowedIps[i]);
+            } catch(FormatException ex)
+            {
+                Console.WriteLine($"Could not parse network '{allowedIps[i]}': {ex.Message}");
+                return;
+            }
+        }
+
+        for(int i = 0; i < disallowed.Length; i++)
+        {
+            try
+            {
+                disallowed[i] = IPNetwork.Parse(disallowedIps[i]);
+            } catch(FormatException ex)
+            {
+                Console.WriteLine($"Could not parse network '{disallowedIps[i]}': {ex.Message}");
+                return;
+            }
+        }
+
+        IPNetwork[] result = Calculator.CalculateAllowedIPs(allowed, disallowed);
+        Console.WriteLine(string.Join<IPNetwork>(", ", result));
     }
 }
